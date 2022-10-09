9a02633dd1bcac421fab518e66371600d26ec1d8
Aster <galaster@foxmail.com>
2022-10-10 01:24:44 +0800

    Hide secret keys

--- projects/ackerman/src/main.rs
+++ projects/ackerman/src/main.rs
@@ -7,25 +7,15 @@ use serde::Deserialize;
 use std::{path::PathBuf, str::FromStr};
 use toml::Value;
 
-fn root_url() -> Url {
-    Url::from_str("https://sandbox.api.sgroup.qq.com/").unwrap()
-}
-
 #[tokio::main]
 async fn main() -> AckermanResult {
     let key = SecretKey::load("projects/ackerman/key.toml").unwrap();
-    // GET /guilds/{guild_id}/channels
+    if key.has_channel_id() {
+    }
+    else {
+        let out = GetGuildResponse::send(&key).await?;
+        println!("{:#?}", out);
+    }
 
-    let request_url = format!("https://sandbox.api.sgroup.qq.com/guilds/{guild_id}/channels", guild_id = key.guild_id);
-    println!("{}", request_url);
-    let response = Client::default()
-        .get(request_url)
-        .header(AUTHORIZATION, format!("Bot {}.{}", key.bot_app_id, key.bot_secret))
-        .send()
-        .await?;
-
-    let out: GetGuildResponse = response.json().await?;
-
-    println!("{:#?}", out);
     Ok(())
 }
--- projects/ackerman/src/runtime/mod.rs
+++ projects/ackerman/src/runtime/mod.rs
@@ -1,20 +1,36 @@
 use crate::{AckermanError, AckermanResult};
+use reqwest::{
+    header::{AUTHORIZATION, USER_AGENT},
+    Client,
+};
 use serde::{Deserialize, Serialize};
-use std::{fs::read_to_string, path::Path};
+use std::{fs::read_to_string, path::Path, time::Duration};
 use toml::from_str;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct SecretKey {
-    pub bot_app_id: u64,
-    pub bot_secret: String,
-    pub bot_token: String,
-    pub guild_id: u64,
-    pub test_channel: u64,
+    bot_app_id: u64,
+    bot_secret: String,
+    bot_token: String,
+    test: ChannelIds,
+    deploy: ChannelIds,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ChannelIds {
+    guild_id: usize,
+    channel_id: usize,
 }
 
 impl SecretKey {
     pub fn load(path: impl AsRef<Path>) -> AckermanResult<Self> {
         Ok(from_str(&read_to_string(path)?)?)
+    }
+    pub fn has_channel_id(&self) -> bool {
+        if cfg!(debug_assertions) { self.test.channel_id != 0 } else { self.deploy.channel_id != 0 }
+    }
+    pub fn bot_token(&self) -> String {
+        format!("Bot {}.{}", self.bot_app_id, self.bot_token)
     }
 }
 
@@ -24,6 +40,27 @@ impl SecretKey {
 pub struct GetGuildResponse {
     code: i32,
     message: String,
+}
+
+impl GetGuildResponse {
+    pub async fn send(key: &SecretKey) -> AckermanResult<Self> {
+        let request_url = if cfg!(debug_assertions) {
+            format!("https://sandbox.api.sgroup.qq.com/guilds/{guild_id}/channels", guild_id = key.test.guild_id)
+        }
+        else {
+            format!("https://api.sgroup.qq.com/guilds/{guild_id}/channels", guild_id = key.deploy.guild_id)
+        };
+        println!("{}", request_url);
+        let response = Client::default()
+            .get(request_url)
+            .header(USER_AGENT, "BotNodeSDK/v2.9.4")
+            .header(AUTHORIZATION, key.bot_token())
+            .timeout(Duration::from_secs(3))
+            .send()
+            .await?;
+        let out: GetGuildResponse = response.json().await?;
+        return Ok(out);
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
