7a3cd481dbe03708b9d78e2082822af9c37f53ad
Aster <galaster@foxmail.com>
2022-10-10 13:06:30 +0800

    Add serde utils function

--- projects/ackerman-qqbot/src/restful/get_message.rs
+++ projects/ackerman-qqbot/src/restful/get_message.rs
@@ -2,26 +2,40 @@ use super::*;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct MessageItem {
-    pub author: Author,
-    pub channel_id: String,
-    pub content: String,
-    pub guild_id: String,
     pub id: String,
-    pub member: Member,
-    pub timestamp: String,
+    pub guild_id: String,
+    pub channel_id: String,
+    pub author: MessageAuthor,
+    pub content: String,
+    pub member: MessageMember,
     pub seq: i64,
+    pub seq_in_channel: String,
+    pub timestamp: String,
+    pub attachments: Vec<MessageAttachment>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Member {
+pub struct MessageMember {
+    pub nick: String,
     pub joined_at: String,
     pub roles: Vec<String>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Author {
-    pub avatar: String,
-    pub bot: bool,
+pub struct MessageAuthor {
     pub id: String,
     pub username: String,
+    pub bot: bool,
+    pub avatar: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct MessageAttachment {
+    id: String,
+    content_type: String,
+    filename: String,
+    height: u32,
+    width: u32,
+    size: u32,
+    url: String,
 }
