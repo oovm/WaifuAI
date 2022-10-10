30ab7f0f7de280cdc0bb5d1af0f171f76e1ffd80
Aster <galaster@foxmail.com>
2022-10-10 09:31:36 +0800

    Pretty print QQBotWebsocket

--- projects/ackerman/src/wss/mod.rs
+++ projects/ackerman/src/wss/mod.rs
@@ -1,32 +1,49 @@
-use std::str::FromStr;
+use std::{
+    fmt::{Debug, Formatter},
+    str::FromStr,
+};
+
 use reqwest::Method;
+use serde::Deserialize;
 use tokio::net::TcpStream;
 use tokio_tungstenite::{connect_async, MaybeTlsStream, WebSocketStream};
-use toml::Value;
 use url::Url;
+
 use crate::{AckermanResult, SecretKey};
 
-#[derive(Debug)]
 pub struct QQBotWebsocket {
     wss: WebSocketStream<MaybeTlsStream<TcpStream>>,
-    info: QQBotConnected
+    connected: QQBotConnected,
+}
+
+impl Debug for QQBotWebsocket {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("QQBotWebsocket").field("config", self.wss.get_config()).field("connected", &self.connected).finish()
+    }
 }
 
 impl QQBotWebsocket {
     pub async fn link(key: &SecretKey) -> AckermanResult<Self> {
         let url = Url::from_str("https://sandbox.api.sgroup.qq.com/gateway/bot")?;
-        let value: Value = key.as_request(Method::GET, url).send().await?.json().await?;
+        let value: QQBotConnected = key.as_request(Method::GET, url).send().await?.json().await?;
         println!("{:#?}", value);
 
-        let (wss, response) = connect_async("wss://sandbox.api.sgroup.qq.com/websocket").await?;
-        Ok(Self {
-            wss,
-            info: QQBotConnected { shards: 0, url: "".to_string() }
-        })
+        let (wss, _) = connect_async(&value.url).await?;
+        Ok(Self { wss, connected: value })
     }
 }
 
+#[derive(Deserialize, Debug)]
 pub struct QQBotConnected {
     shards: u32,
     url: String,
+    session_start_limit: SessionStartLimit,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct SessionStartLimit {
+    max_concurrency: u32,
+    remaining: u32,
+    reset_after: u32,
+    total: u32,
 }
